c991537be6c4345b51f041db06d70b59093accee
Jennie Dallas <jennied15@outlook.com>
2024-11-02 21:59:28 -0600

    progress save....although 'progress' is generous

---
+++ src/LetterKey.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LetterKey = (zhuyinLetter) => {
+    return (
+        <div className="letterKey">
+            <p>{zhuyinLetter}</p>
+        </div>
+    )
+}
---
+++ src/ZhuyinDictionary.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Zhuyin = { 1 : 'ㄅ', 
+                 2 : 'ㄆ', 
+                 3 : 'ㄇ', 
+                 4 : 'ㄈ', 
+                 5 : 'ㄉ', 
+                 6 : 'ㄊ', 
+                 7 : 'ㄋ'};
