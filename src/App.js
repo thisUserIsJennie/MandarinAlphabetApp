d558bc2b7a895c3cf0dc96623586d6b724cf7b7c
Jennie Dallas <jennied15@outlook.com>
2024-11-03 22:28:59 -0700

    we have a nav bar!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './customLogo.jpg';
 import './App.css';
 import MainPage from './MainPage';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* TODO: don't hardcode page selection values */
-const PageSelection = {HomePage: "HomePage"};
+const PageSelection = { HomePage: "HomePage" };
 
 const App = () => {
   var userSelectedContent = PageSelection.HomePage;
@@ -12,35 +17,40 @@ const App = () => {
 
   return (
     <div className="App">
-      {mainContent};
       {sidebar};
+      {mainContent};
     </div>
   );
 }
 
 const Sidebar = () => {
   return (
-    <div id="sidebar">
-      <div class="inner">
-        <nav id="menu">
-          <header class="major">
-            <h2>Menu</h2>
-          </header>
-          <ul>
-            <li><a href="index.html">Homepage</a></li>
-            <li>
-              <span class="opener">Features Coming Soon</span>
-              <ul>
-                <li><a href="#">Quiz</a></li>
-                <li><a href="#">Learn To Write Zhuyin</a></li>
-                <li><a href="#">Interactive Keyboard</a></li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-
+    <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <NavDropdown title="Coming Soon" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Quiz</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Learn To Write Zhuyin
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Interactive Keyboard</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
